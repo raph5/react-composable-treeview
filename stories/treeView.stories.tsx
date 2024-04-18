6c71bb7229797e7e9b7d2bcb1b9750337878d030
raph5 <raphguyader@gmail.com>
2024-04-18 22:49:53 +0200

    package.json preparation for publication and story update

--- stories/treeView.stories.tsx
+++ stories/treeView.stories.tsx
@@ -6,18 +6,20 @@ export default {
   title: 'TreeView'
 } as Meta
 
-export const Simple: StoryObj = {
+export const FileExplorer: StoryObj = {
   render: () => (
     <TreeView.Root>
-      <TreeView.Group value="src">
-        <TreeView.Trigger>src</TreeView.Trigger>
+      <TreeView.Group value="/src">
+        <TreeView.Trigger>
+          src
+        </TreeView.Trigger>
         <TreeView.Content>
-          <TreeView.Item value="src/index.ts">index.ts</TreeView.Item>
-          <TreeView.Item value="src/treeView.tsx">treeView.tsx</TreeView.Item>
+          <TreeView.Item value="/src/index.ts">index.ts</TreeView.Item>
+          <TreeView.Item value="/src/treeView.tsx">treeView.tsx</TreeView.Item>
         </TreeView.Content>
       </TreeView.Group>
-      <TreeView.Item value="tsconfig.json">tsconfig.json</TreeView.Item>
-      <TreeView.Item value="package.json">package.json</TreeView.Item>
+      <TreeView.Item value="/tsconfig.json">tsconfig.json</TreeView.Item>
+      <TreeView.Item value="/package.json">package.json</TreeView.Item>
     </TreeView.Root>
   )
 }
