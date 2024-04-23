4bca1e8753921940fa7909cff2145b7c0dd3d6e6
raph5 <raphguyader@gmail.com>
2024-04-23 16:12:32 +0200

    event composer fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,26 @@
+// @ts-nocheck
 
 export function composeEventHandlers<E>(
   userHandler?: (event: E) => void,
   libHandler?: (event: E) => void
 ) {
   return function handleEvent(event: E) {
+    const prevented = event.defaultPrevented
+    if(prevented) {
+      event.defaultPrevented = false
+    }
+
     userHandler?.(event)
 
-    // @ts-ignore
-    if (!(event).defaultPrevented) {
-      return libHandler?.(event)
+    let returnValue
+    if (!event.defaultPrevented) {
+      returnValue = libHandler?.(event)
+
+      if(prevented) {
+        event.defaultPrevented = true
+      }
     }
+
+    return returnValue
   };
 }
